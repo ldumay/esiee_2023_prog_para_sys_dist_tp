09a3a6b6ebee6d4d0554d9130aea2b7085ade116
ldumay <dumay.famille@gmail.com>
2023-02-23 17:24:50 +0100

    Update reduction_tab.c

--- tpNote/exercice2/reduction_tab.c
+++ tpNote/exercice2/reduction_tab.c
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-// reduction_tab.c ---  réduction d’un tableau
+// reduction_tab.c ---  rÃ©duction dâ€™un tableau
 //
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
 #include <string.h>
 
 
@@ -24,66 +25,62 @@
 
 // Initialisation a zero d'un tableau
 void clear(double* array, int size) {
-  for(int l=0; l<size; l++) array[l] = 0;
+	for(int l=0; l<size; l++) array[l] = 0;
 }
 
 int main() {
 
-  const int nmolec=VAL_NMOLEC, nmol=VAL_NMOL, n=VAL_N;
+	const int nmolec=VAL_NMOLEC, nmol=VAL_NMOL, n=VAL_N;
+	double tab[nmolec][n][nmol];
 
-  double tab[nmolec][n][nmol];
-  // Initialisation du tableau
-  for(int k=0; k<nmolec; k++) {
-    for(int j=0; j<n; j++) {
-      for(int i=0; i<nmol; i++) {
-        tab[k][j][i] = i + j + k;
-      }
-    }
-  }
+	// Initialisation du tableau
+	for(int k=0; k<nmolec; k++) {
+		for(int j=0; j<n; j++) {
+			for(int i=0; i<nmol; i++) {
+				tab[k][j][i] = i + j + k;
+			}
+		}
+	}
 
-  double tab1[nmol], tab2[nmol];
-  clear(tab2, nmol);
-
- 
-  // Nid de boucle a paralleliser
-  for(int k=0; k<nmolec; k++) {
-    clear(tab1, nmol);
-
-    for(int j=0; j<n; j++) {
-      for(int i=0; i<nmol; i++) {
-        tab1[i] += tab[k][j][i];
-      }
-    }
-
-    for(int l=0; l<nmol; l++) tab2[l] += 2*tab1[l];
-  }
-
-  
-  // Verification des resultats
-  double tab1c[nmol], tab2c[nmol];
-  clear(tab2c, nmol);
-  for(int k=0; k<nmolec; k++) {
-    clear(tab1c, nmol);
-
-    for(int j=0; j<n; j++) {
-      for(int i=0; i<nmol; i++) {
-         tab1c[i] += tab[k][j][i];
-      }
-    }
-
-    for(int l=0; l<nmol; l++) tab2c[l] += 2*tab1c[l];
-  }
-
-  double err = 0;
-  for(int i=0; i<nmol; i++) {
-    double curr = fabs(tab2c[i] - tab2[i]) / fabs(tab2c[i]);
-    if (curr > err)
-      err = curr;
-  }
-
-  // Impression du resultat
+	double tab1[nmol], tab2[nmol];
+	clear(tab2, nmol);
 
 
-  return EXIT_SUCCESS;
+	// Nid de boucle a paralleliser
+	#pragma omp parallel for private(i,j) {
+		for(int k=0; k<nmolec; k++) {
+			clear(tab1, nmol);
+			for(int j=0; j<n; j++) {
+				for(int i=0; i<nmol; i++) {
+					tab1[i] += tab[k][j][i];
+					tab1[i] = 2*tab1[i];
+				}
+			}
 
+		}
+	}
+
+	// Verification des resultats
+	double tab1c[nmol], tab2c[nmol];
+	clear(tab2c, nmol);
+
+	for(int k=0; k<nmolec; k++) {
+		clear(tab1c, nmol);
+		for(int j=0; j<n; j++) {
+			for(int i=0; i<nmol; i++) {
+				tab1c[i] += tab[k][j][i];
+			}
+		}
+		for(int l=0; l<nmol; l++) tab2c[l] += 2*tab1c[l];
+	}
+
+	double err = 0;
+	for(int i=0; i<nmol; i++) {
+	double curr = fabs(tab2c[i] - tab2[i]) / fabs(tab2c[i]);
+	if (curr > err)
+		err = curr;
+	}
+
+	// Impression du resultat
+	return EXIT_SUCCESS;
 }
