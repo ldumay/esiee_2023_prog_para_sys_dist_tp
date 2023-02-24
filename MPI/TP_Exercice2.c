c10e912428509723ca35b448a3b0e755f9622799
zchen <zchen@free-mobile.fr>
2023-02-24 17:30:12 +0100

    exo 2 done

--- MPI/TP_Exercice2.c
+++ MPI/TP_Exercice2.c
@@ -3,47 +3,44 @@
 # include <stdlib.h>
 
 # define MPI_TAG_VALUE 123456
-
+	
 void run( const int myid,const int nprocs) {
-	if ( myid == 0) {
-		// Create an array
-		int* valtx;
-        // Allocate memory for the array
-        valtx = (int*) malloc(nprocs * sizeof(int));
-        // Initialize the array
-        for (int i = 1; i < nprocs; i++) {
-            valtx[i] = i*10;
-        }
-        // Send the array
-        for (int dest = 1; dest < nprocs; dest++) {
-			// Send the value
-            MPI_Send(&valtx[dest] ,1, MPI_INT ,dest, MPI_TAG_VALUE, MPI_COMM_WORLD);
-            //printf("proc %d sent the value %d to proc %d\n",myid,valtx,source);
-		}
-	}
+   if ( myid == 0) {
+      
+      int* valtx;
 
-	if ( myid != 0) {
-		int valrx;
-		MPI_Status status;
-		MPI_Recv(&valrx, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		printf("proc %d received the value %d\n",myid,valrx);
-		//printf("proc %d received the tag %d\n",myid,status.MPI_TAG);
-		printf("proc %d received the source %d\n",myid,status.MPI_SOURCE);
-		//printf("proc %d received the error %d\n",myid,status.MPI_ERROR);
-        printf("proc %d received the source %d with the value %d\n",myid,status.MPI_SOURCE,valrx);
-	}
+     // Allocate memory for the array
+      valtx = (int*) malloc(nprocs * sizeof(int));
+
+      // Initialize the array
+      for (int i = 1; i < nprocs; i++) {
+         valtx[i] = i*10;
+      }
+      
+      for (int dest = 1; dest < nprocs; dest++) {
+         MPI_Send(&valtx[dest] ,1, MPI_INT ,dest, MPI_TAG_VALUE, MPI_COMM_WORLD);
+      }
+   }
+
+   if ( myid != 0) {
+      int valrx;
+      MPI_Status status;
+      MPI_Recv(&valrx ,1, MPI_INT,0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+      printf("proc %d received the source %d with the value %d\n",myid,status.MPI_SOURCE,valrx);
+   }
 }
 
 int main(int argc , char *argv []) {
-	int myid;
-	int nprocs;
+   int myid;
+   int nprocs;
+   MPI_Init(&argc, &argv);
+  
+   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+   run(myid,nprocs);
 
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-
-	run(myid,nprocs);
-
-	MPI_Finalize();
-	return 0;
+   MPI_Finalize();
+   return 0;
 }
+
