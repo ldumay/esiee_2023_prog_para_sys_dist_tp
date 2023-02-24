3c6b500b07f578018fe1d544ad76a65a12759cca
Maxence <maxence.hennekein@orange.fr>
2023-02-24 18:21:51 +0100

    Avancement exo 4
    J'ai dû refaire la méthodologie pour permettre le calcul matriciel
    J'ai pricnipalement ajouté des commentaires sur ce qu'il y a à finir

--- MPI/04-mpi_mm_skel.c
+++ MPI/04-mpi_mm_skel.c
@@ -11,9 +11,9 @@
 #include <malloc.h>
 
 
-#define NRA 62                 /* number of rows in matrix A */
-#define NCA 15                 /* number of columns in matrix A */
-#define NCB 7                  /* number of columns in matrix B */
+#define NRA 8                 /* number of rows in matrix A */
+#define NCA 4                 /* number of columns in matrix A */
+#define NCB 6                  /* number of columns in matrix B */
 #define MASTER 0               /* taskid of first task */
 #define FROM_MASTER 1          /* setting a message type */
 #define FROM_WORKER 2          /* setting a message type */
@@ -37,6 +37,8 @@ int main (int argc, char *argv[]) {
 		    err, errsq;            /* error and square error of the matrix product */
 
 	MPI_Status status;
+	// status of all request
+	MPI_Request* requests;
 
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
@@ -86,21 +88,50 @@ int main (int argc, char *argv[]) {
 		/* Send matrix data to the worker tasks: */
 
 		/* 1. Calculate how many rows per worker */
+		int worker_task[NRA];
+		int worker_num = 0;
+		for (i=0; i<NRA; i++) {
+			if (i == MASTER) {
+				// Move in table
+				worker_num = (++worker_num) % numtasks;
+			}
+			worker_task[i] = worker_num;
+			// Move in table
+			worker_num = (++worker_num) % numtasks;
+		}
 		/* 2. For each worker, send:
 		 offset, 
 		 number of rows, 
 		 chunk of a of size "number of rows", 
-		 a whole copy of b
-		*/ 
-
+		 a whole copy of b -> broadcast
+		*/
 		mtype = FROM_MASTER;
+		worker_num = 0;
+		// Send in broadcast B
+		// MPI_Broad();
+		// Send a portition of A to each process
+		for (i=0; i < NCA; i++) {
+			worker_num++;
+			if (i == MASTER) {
+				worker_num--;
+				continue;
+			}
+			rows = NCA;
+			printf("Task %d\twork on %d rows\n", i, rows);
+			MPI_Send(&rows, 1, MPI_INT, i, mtype, MPI_COMM_WORLD);
+			double* worker_task = (double*)malloc(rows * sizeof(double));
+			MPI_Send(worker_task, rows, MPI_DOUBLE, i, mtype, MPI_COMM_WORLD);
+		}
+
+		// Wait for all requests to finish
+		// for requests == FINISH
 
 		/* Receive results from worker tasks */
 		mtype = FROM_WORKER;
 
+		// Allocation matrice C
 		c = (double**)malloc(NRA*sizeof(double*));
 		double *tc = (double *)malloc(NRA * NCB * sizeof(double));	
-
 		for (i=0;i<NRA;i++) {
 			c[i] = &(tc[i*NCB]);
 		}
@@ -110,6 +141,9 @@ int main (int argc, char *argv[]) {
 			number of rows to receive,
 			chunk of c of size "rows to receive",
 		*/
+		// Receive all C part from workers
+		// Place all workers result into a
+		// c[nb_row] = matrice_from_worker
 
 		/* Print results */
 		printf("******************************************************\n");
@@ -142,14 +176,14 @@ int main (int argc, char *argv[]) {
 
 
 	/**************************** worker task ************************************/
-	if (taskid > MASTER) {
+	else {
 		mtype = FROM_MASTER;
 		/* 
 		Receive from master:
 		   offset,
 		   number of rows
       	*/
-
+		MPI_Recv(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
 
 		// Allocate space for the chunk of a received and for the whole b matrix
 		a = (double**)malloc(rows*sizeof(double*));
@@ -165,8 +199,11 @@ int main (int argc, char *argv[]) {
 		for (i=0;i<NCA;i++) {
 			b[i] = &(tb[i*NCB]);
 		}
-		/* Receive chunk of a and whole b */
 
+		/* Receive chunk of a and whole b */
+		// Temporary use of t due to data type
+		MPI_Recv(t, rows, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &status);
+		printf("%d\tReceive data from %d of size %d\n", taskid, MASTER, rows);
 
 		// Allocate space for the chunk of c to calculate 
 		c = (double**)malloc(rows*sizeof(double*));
@@ -176,6 +213,14 @@ int main (int argc, char *argv[]) {
 			c[i] = &(tc[i*NCB]);
 		}
 		/* Calculate the matrix product */
+		// To calculate, go into each a element and multiply by b column
+		for(i=0;i<rows;i++){
+			for(j=0;j<NCB;k++) {
+				for(k=0;k<NCA;k++) {
+					c[i][j] += a[i][k] + b[k][j];
+				}
+			}
+		}
 
 		/* Send offset, number of rows and chunk of c to the master*/ 
 		mtype = FROM_WORKER;
