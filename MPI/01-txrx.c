74e05d829fed6dcfbf2ea102211f9a58332ce323
zchen <zchen@free-mobile.fr>
2023-02-24 15:30:50 +0100

    Question 2 done

--- MPI/01-txrx.c
+++ MPI/01-txrx.c
@@ -3,31 +3,34 @@
 
 # define MPI_TAG_VALUE 123456
 	
-void run( const int myid) {
+void run( const int myid,const int nprocs) {
    if ( myid == 0) {
       int valtx;
+
       valtx = 3785;
 
-      MPI_Send(&valtx ,1, MPI_INT ,1, MPI_TAG_VALUE, MPI_COMM_WORLD);
-      valtx++;
-      MPI_Send(&valtx ,1, MPI_INT ,2, MPI_TAG_VALUE, MPI_COMM_WORLD);
-      valtx++;
-      MPI_Send(&valtx ,1, MPI_INT ,3, MPI_TAG_VALUE, MPI_COMM_WORLD);
+      for (int source = 1; source < nprocs; source++) {
+         MPI_Send(&valtx ,1, MPI_INT ,source, MPI_TAG_VALUE, MPI_COMM_WORLD);
+         valtx++;
+      }
    }
-   else { 
+
+   if ( myid != 0) {
       int valrx;
       MPI_Status status;
       MPI_Recv(&valrx ,1, MPI_INT,0, MPI_TAG_VALUE, MPI_COMM_WORLD, &status);
-      printf("proc %d received the value %d\n",myid,valrx);
+      printf("proc %d received the value %d\n",myid,valrx); 
    }
 }
 
 int main(int argc , char *argv []) {
    int myid;
+   int nprocs;
    MPI_Init(&argc, &argv);
   
    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-   run(myid);
+   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+   run(myid,nprocs);
 
    MPI_Finalize();
    return 0;
